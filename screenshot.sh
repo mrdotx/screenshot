62718ba560632f6f6ff5de7c07835c03d703ce02
mrdotx <klassiker@gmx.de>
2020-10-30 23:18:50 +0100

    notification structure update

--- screenshot.sh
+++ screenshot.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/screenshot/screenshot.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/screenshot
-# date:       2020-10-18T11:34:35+0200
+# date:       2020-10-30T23:12:41+0100
 
 # config
 screenshot_directory=$HOME/Schreibtisch
@@ -39,14 +39,19 @@ case "$1" in
         printf "%s\n" "$help"
         ;;
     -desk)
-        $execute && $screenshot_preview
+        $execute \
+            && $screenshot_preview
         ;;
     -window)
-        $execute -i "$(xdotool getactivewindow)" && $screenshot_preview
+        $execute -i "$(xdotool getactivewindow)" \
+            && $screenshot_preview
         ;;
     -select)
-        notify-send "maim" "select an area or window to screenshot" \
-            & $screenshot_command -s && $screenshot_preview
+        notify-send \
+            "maim" \
+            "select an area or window to screenshot" \
+        & $screenshot_command -s \
+        && $screenshot_preview
         ;;
     *)
         printf "%s\n" "$help"
